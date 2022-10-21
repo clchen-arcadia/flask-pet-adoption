a4a7e225ebfdadf2165d8d2b2f4ecf0e0a2f302c
michael-bocim <mtbocim@gmail.com>
2022-10-21 16:08:00 -0600

    created add pet form and route

--- forms.py
+++ forms.py
@@ -1,1 +1,21 @@
-"""Forms for adopt app."""
+from random import choices
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, SelectField
+
+
+class AddPetForm(FlaskForm):
+    """
+        Form for adding a new pet for adoption.
+    """
+
+    name = StringField("Pet Name")
+    species = StringField("Species")
+    photo_url = StringField("Photo URL")
+    age = SelectField("Age",
+                      choices=[
+                          ('baby', 'Baby'),
+                          ('young', 'Young'),
+                          ('adult', 'Adult'),
+                          ('senior', 'Senior')
+                      ])
+    notes = StringField("Notes")
